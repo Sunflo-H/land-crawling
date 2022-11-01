e623387fe6f486a6faae3557b4c5c80d13cca2d8
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-11-02 05:56:13 +0900

    추가 매물정보 적용 옵션코드, 관리비코드  적용

--- 직방.py
+++ 직방.py
@@ -100,20 +100,69 @@ def parseRoomInfo( room_info, find_text=None ):
     parsed_data = {
         '제목': room_info['item'].get('title'),
         '주소': room_info['item'].get('address'),
-        '지번주소':  room_info['item'].get('jibunAddress'),
+        '지번주소': room_info['item'].get('jibunAddress'),
+
+        # 좌표가 정확하진 않아.. 마커클러스터할땐 이거쓰고, 상세하게 보여줄때는 지번주소로 검색
+        '좌표': room_info['item'].get('random_location'), 
         '정보': {
             '사진': room_info['item'].get('images'),
             '썸네일': room_info['item'].get('image_thumbnail'),
             '전세/월세': room_info['item'].get('sales_type'),
             '방': room_info['item'].get('service_type'),
-            '층수': room_info['item'].get('floor'),
-            '총층수': room_info['item'].get('floor_all')
-        }
+            '층수': room_info['item'].get('floor'), # '1','2','3', '반지하', '옥탑방' 
+            '총층수': room_info['item'].get('floor_all'),
+            '상세내용': room_info['item'].get('description'),
+
+            # 타입 코드 :
+            # 원룸인경우 01 = 분리형, 02 = 오픈형, 03 = 복층형, 04 = 투룸, 05 = 쓰리룸+, 06 = 포룸+
+            '타입': room_info['item'].get('room_type'),
+            '화장실': room_info['item'].get('bathroom_count'),
+            '주택타입': room_info['item'].get('residence_type'), # 다세대주택, 주택, 공동주택
+
+            #'주차가능', '1대가능' 이런식으로 써있다. 그대로 이용하면 될듯
+            '주차': room_info['item'].get('parking'),
+            '엘리베이터': room_info['item'].get('elevator'), # true / false
+            '즉시입주': room_info['item'].get('movein_date'),
+            '준공': room_info['item'].get('approve_date'), # 얘는 날짜 타입이 제각각... 따로 조정필요
+            '방향': room_info['item'].get('room_direction'), # "S" ,"SW" 이런식
+
+            #  확정 01 에어컨,   02 = 냉장고,     03 세탁기,   04 = 가스레인지,
+            #       05 = 인덕션, 06 = 전자레인지, 07 = 책상,   08 = 책장, 
+            #       09 = 침대,   10 = 옷장,       11 = 신발장, 12 = 싱크대
+            '옵션': room_info['item'].get('options') 
+        },
+        '비용':{
+            '관리비': room_info['item'].get('manage_cost'),
+            
+            # 관리비 옵션코드는 같은 코드를 공유한다.
+            #  확정 03 = 수도, 04 = 인터넷, 05 = 티비
+            '관리비포함': room_info['item'].get('manage_const_inc'),
+
+            # 01 02 03 전기 가스 수도
+            # 01 02 03 04 05 전기, 가스, 수도, 인터넷, 티비
+            '관리비별도': room_info['item'].get('manage_const_not_inc'),
+            '보증금액': room_info['item'].get('보증금액'),
+            '월세금액': room_info['item'].get('월세금액')
+        },
+        '면적':{
+            '공급면적_m2': room_info['item'].get('공급면적_m2'),
+            '대지권면적_m2': room_info['item'].get('대지권면적_m2'),
+            '전용면적_m2': room_info['item'].get('전용면적_m2')
+        },
+        '중개사': {
+            '이름': room_info['agent'].get('agent_title'),
+            '주소': room_info['agent'].get('agent_address'),
+            '전화': room_info['agent'].get('agent_phone'),
+            '소개': room_info['agent'].get('agent_intro'),
+            '직원정보': room_info['agent'].get('owner'),
+        },
+        '지하철': room_info['subways']
     }
-    if find_text is not None:
-        if  parsed_data["설명"]["상세내용"].find( find_text ) > 0:
-            return parsed_data
-        return None
+
+    # if find_text is not None:
+    #     if  parsed_data['설명']['상세내용'].find( find_text ) > 0:
+    #         return parsed_data
+    #     return None
     return parsed_data
  
 방_리스트 = [] 
@@ -140,15 +189,13 @@ def 크롤링(subway_name):
         
     else:
         return
-                 
-
 
 # 크롤링 실행 ( 테스트 성공하고, 파일에 데이터를 저장하고 싶다면 이 함수를 사용)
 def start():
     for subway in 지하철역리스트:
         크롤링(subway)
         file_name = '{subway_name}.txt'
-        f = open(file_name.format(subway_name = subway),'w', encoding="UTF-8")
+        f = open(file_name.format(subway_name = subway),'w', encoding='UTF-8')
         str = repr(방_리스트)
         f.write(str)
         f.close()
